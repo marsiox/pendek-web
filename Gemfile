ff0ae8dba161a400c2e7377edc6ff5477c855e3f
Krzyś Marszałek <krzys.marszalek@gmail.com>
2017-08-21 18:00:38 +0700

    Application scaffold with config and basic api calls

--- Gemfile
+++ Gemfile
@@ -6,9 +6,11 @@ git_source(:github) do |repo_name|
 end
 
 gem 'sinatra', '~> 1.4', '>= 1.4.7'
+gem 'sinatra-contrib'
 gem 'rspec-rails', '~> 3.4', '>= 3.4.2'
 gem 'slim'
 gem 'json'
+gem 'rest-client'
 
 group :development, :test do
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
--- pendek.rb
+++ pendek.rb
@@ -1,10 +1,34 @@
 require 'sinatra'
+require "sinatra/config_file"
 require 'slim'
+require "net/http"
+require "uri"
+require 'json'
+require 'ostruct'
+
+config_file 'config/application.yml'
 
 get '/' do
+  uri = URI.parse(settings.api["base"] + "/urls")
+  response = Net::HTTP.get_response(uri)
+  @urls = OpenStruct.new(JSON.parse(response.body))
+
   slim :index
 end
 
 post '/' do
-  slim :index
+  uri = URI.parse(settings.api["base"] + "/urls")
+  http = Net::HTTP.new(uri.host, uri.port)
+  request = Net::HTTP::Post.new(uri.path, initheader = {"Content-Type" => "application/json"})
+  request.body = { data: { full: params["url"] } }.to_json
+  response = http.request(request)
+  redirect "/"
+end
+
+get '/:short' do
+  uri = URI.parse(settings.api["base"] + "/stats/#{params[:short]}")
+  response = Net::HTTP.get_response(uri)
+  @stats = OpenStruct.new(JSON.parse(response.body))
+
+  slim :stats
 end
